440edbda0d7d15c7719c08eb3c3687b41bb2de94
Petko <pavlovskipetko@abv.bg>
2021-10-26 00:01:32 +0300

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './styles.scss';
+import logo from "./images/logo.png";
+import ferris from "./images/ferris-wizard.png"
 
 function App() {
-  return (
-    <div>hello</div>
-  );
+    return (
+        <>
+            <div className="header">
+                <img src={logo} alt="nixum-logo"/>
+            </div>
+            <div className="content">
+                <span>Delivering high-quality Rust solutions for the Blockchain world.</span>
+                <span>For inquiries, reach out <a href={"mailto:nixumltd@gmail.com"} className="accent">here</a>.</span>
+            </div>
+            <div className="mascot">
+                <img src={ferris} alt="mascot"/>
+            </div>
+        </>
+    );
 }
 
 export default App;
